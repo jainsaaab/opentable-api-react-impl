8fc2c2f94c8393c9ab13312651c0a601843a53b0
Aditya Jain <jainsaab@protonmail.com>
2020-07-10 16:31:10 +0530

    Updates

---
+++ precache-manifest.1e6c61b3b297ac62ec0be38714801cb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa3022d9a43cb48555f6aa52b04ca39",
+    "url": "/opentable-api-react-impl/index.html"
+  },
+  {
+    "revision": "4c3c4729a4e3d7254bf4",
+    "url": "/opentable-api-react-impl/static/css/main.ccd02ecf.chunk.css"
+  },
+  {
+    "revision": "b220ade35fffbbd7e9f7",
+    "url": "/opentable-api-react-impl/static/js/2.02da841d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/opentable-api-react-impl/static/js/2.02da841d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3c4729a4e3d7254bf4",
+    "url": "/opentable-api-react-impl/static/js/main.2e7e1787.chunk.js"
+  },
+  {
+    "revision": "6fe562bc1b54fafcf5e2",
+    "url": "/opentable-api-react-impl/static/js/runtime-main.1785d0a4.js"
+  },
+  {
+    "revision": "76ea32123dd9bda4a2fb3a8e783924bc",
+    "url": "/opentable-api-react-impl/static/media/logo.76ea3212.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/opentable-api-react-impl/static/media/spinner.235b9b3e.gif"
+  }
+]);
